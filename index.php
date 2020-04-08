b8a3613fef887fe2f076326c0db471c405d1c483
Dock0d1 <yuryzinho@hotmail.com>
2020-04-07 22:29:41 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,29 +1,80 @@
 <?php 
 
+session_start();
+
 require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
-	
 	$page = new Page();
-
 	$page->setTpl("index");
-
 });
 
 $app->get('/admin', function() {
-	
+	User::verifyLogin();
 	$page = new PageAdmin();
-
 	$page->setTpl("index");
+});
 
+$app->get('/admin/login', function() {
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page ->setTpl("login");
+});
+
+$app->post('/admin/login', function () {
+	User::login($_POST["login"], $_POST["password"]);
+	header("location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function() {
+	User::logout();
+	header("location: /admin/login");
+});
+
+$app->get("/admin/users/:iduser", function($iduser){
+	User::verifyLogin();
+});
+
+$app->get("/admin/users", function(){
+	User::verifyLogin();
+	$users = User::listAll();
+	$page = new PageAdmin();
+	$page ->setTpl("users");
+});
+
+$app->get("/admin/users/:iduser", function($idser){
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page ->setTpl("users-update");
+});
+
+$app->get("/admin/users/create", function(){
+	User::verifyLogin();
+	$page = new PageAdmin();
+	$page ->setTpl("users-create", array(
+		"users"=>users
+
+	));
+});
+
+$app->post("/admin/users/create", function(){
+	User::verifyLogin();
+});
+
+$app->post("/admin/users/:iduser", function($iduser){
+	User::verifyLogin();
 });
 
 $app->run();
